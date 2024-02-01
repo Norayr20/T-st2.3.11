6ded7ef2702c64220b89b504ddc440704f5583d8
Norayr <asoyannorayr99@gmail.com>
2024-02-01 16:19:04 +0300

    add commit

--- src/house/Human.java
+++ src/house/Human.java
@@ -1,5 +1,34 @@
 package house;
+
 public class Human {
+    private String secret;
+    String news;
+    protected String experience;
+    public String gossip;
+
+    public Human(String secret, String news, String experience, String gossip) {
+        this.secret = secret;
+        this.news = news;
+        this.experience = experience;
+        this.gossip = gossip;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    protected String getExperience() {
+        return experience;
+    }
+
+    public String getGossip() {
+        return gossip;
+    }
+
     public static void main(String[] args) {
         Human person = new Human("My secret", "Latest news", "Great experience", "Interesting gossip");
 
@@ -8,32 +37,4 @@ public class Human {
         System.out.println("Experience: " + person.getExperience());
         System.out.println("Gossip: " + person.getGossip());
     }
-
-        private final String secret;
-        private final String news;
-        private final String experience;
-        private final String gossip;
-
-        public Human(String secret, String news, String experience, String gossip) {
-            this.secret = secret;
-            this.news = news;
-            this.experience = experience;
-            this.gossip = gossip;
-        }
-
-        public String getSecret() {
-            return secret;
-        }
-
-        public String getNews() {
-            return news;
-        }
-
-        String getExperience() {
-            return experience;
-        }
-
-        String getGossip() {
-            return gossip;
-        }
-    }
+}
