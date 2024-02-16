4601d784342576e4edf2b72db5b7b8f6e5c2267c
Norayr <asoyannorayr99@gmail.com>
2024-02-16 15:11:53 +0300

    add

--- src/house/Human.java
+++ src/house/Human.java
@@ -1,39 +1,32 @@
 package house;
 public class Human {
-        private String secret;
-        String news;
-        protected String experience;
-        public String gossip;
-
-        public Human(String secret, String news, String experience, String gossip) {
-            this.secret = secret;
-            this.news = news;
-            this.experience = experience;
-            this.gossip = gossip;
-        }
-
-        public String getSecret() {
-            return secret;
-        }
-
-        public String getNews() {
-            return news;
-        }
-
-        protected String getExperience() {
-            return experience;
-        }
-
-        public String getGossip() {
-            return gossip;
-        }
-
-        public static void main(String[] args) {
-            Human person = new Human("My secret", "Latest news", "Great experience", "Interesting gossip");
-
-            System.out.println("Secret: " + person.getSecret());
-            System.out.println("News: " + person.getNews());
-            System.out.println("Experience: " + person.getExperience());
-            System.out.println("Gossip: " + person.getGossip());
-        }
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.news);
+        System.out.println(human.experience);
+        System.out.println(human.gossip);
+        System.out.println(human.getNews());
+        System.out.println(human.getExperience());
+        System.out.println(human.getGossip());
     }
+    private String secret;
+    protected String news;
+    String experience;
+    public String gossip;
+
+    private String getSecret() {
+        return secret;
+    }
+
+    protected String getNews() {
+        return news;
+    }
+
+    String getExperience() {
+        return experience;
+    }
+
+    public String getGossip() {
+        return gossip;
+    }
+}
