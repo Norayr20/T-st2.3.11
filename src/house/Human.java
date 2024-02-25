a4261744fa575b4fee1f85cb3c650d7a745966cb
Norayr <asoyannorayr99@gmail.com>
2024-02-25 17:36:37 +0300

    add

--- src/house/Human.java
+++ src/house/Human.java
@@ -1,14 +1,6 @@
 package house;
+
 public class Human {
-    public static void main(String[] args) {
-        Human human = new Human();
-        System.out.println(human.news);
-        System.out.println(human.experience);
-        System.out.println(human.gossip);
-        System.out.println(human.getNews());
-        System.out.println(human.getExperience());
-        System.out.println(human.getGossip());
-    }
     private String secret;
     protected String news;
     String experience;
